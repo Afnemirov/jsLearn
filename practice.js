6c7461b488a0037a76ca88f70b0a9d61b4960daa
Unknown <afnemirov@gmail.com>
2017-05-14 16:46:56 +0300

    Update

--- practice.js
+++ practice.js
@@ -1,1 +1,5 @@
-alert('Shalom');
+var input = prompt("Please type a number");
+var topNumber = parseInt(input);
+var randomNumber = Math.floor(Math.random() * topNumber) + 1;
+var message = "<h2>" + randomNumber + " is a number between 1 and " + topNumber + ".</h2>";
+document.write(message);
